6fe077da1faa1acd2d8b5eb8165d2e4d4888282d
TanVuong <tanvuong222@gmail.com>
2022-05-05 21:03:17 +0700

    File fontend trong folder Public

---
+++ public/frontend/TanVuong/scripts.js
@@ -0,0 +1,14 @@
+/* Toggle menu */
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight === "0px")
+        {
+            MenuItems.style.maxHeight = "200px";
+        }
+    else{
+        MenuItems.style.maxHeight = "0px";
+    }
+}
