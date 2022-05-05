52474c6658656f4b367e3aacd34507599a04b3b9
TanVuong <tanvuong222@gmail.com>
2022-05-05 20:43:47 +0700

    File font end nằm trong folder public nha

---
+++ public/frontend/scripts.js
@@ -0,0 +1,22 @@
+/* Toggle menu */
+
+function menutoggle(){
+    var MenuItems = document.getElementById("MenuItems");
+    
+    MenuItems.style.maxHeight = "0px";
+
+    if(MenuItems.classList.contains("active")){
+        MenuItems.classList.remove("active");
+    }else{
+        MenuItems.classList.add("active");
+    }
+
+    if(MenuItems.style.maxHeight === "0px")
+    {
+        MenuItems.style.maxHeight = "200px";
+    }
+    else{
+        MenuItems.style.maxHeight = "0px";
+        // MenuItems.hide();
+    }
+}
